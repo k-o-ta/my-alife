977525346f5b08fb9c89b7da027054c5c1b55910
Koji Ota <afterjnih@gmail.com>
2018-09-03 00:48:49 +0900

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,12 @@ extern crate rand;
 
 // use glutin::dpi::*;
 // use glutin::GlContext;
-use glium::texture::{PixelValue, Texture2dDataSource};
 use ndarray::prelude::*;
 use ndarray::Array;
 use ndarray::Array2;
 use ndarray_rand::F32;
 use ndarray_rand::RandomExt;
 use num::Integer;
-use num_traits::CheckedNeg;
 use rand::distributions::Range;
 use std::ops::AddAssign;
 
@@ -34,41 +32,43 @@ const DV: f32 = 1e-5;
 const F: f32 = 0.04;
 const K: f32 = 0.06;
 
+type Matrix<T> = ndarray::ArrayBase<ndarray::OwnedRepr<T>, ndarray::Dim<[usize; 2]>>;
+
 fn main() {
-    roll2();
     let (u, v) = make_matrix();
-    // println!("{:?}", u);
     draw_triangle(u, v);
 }
 
 fn make_texture_image<'a>(
     u: &ndarray::ArrayBase<ndarray::OwnedRepr<f32>, ndarray::Dim<[usize; 2]>>,
 ) -> glium::texture::RawImage2d<'a, u8> {
-    let vector = u.iter().map(|d| d).collect::<Vec<&f32>>();
     let mut texture_data = Vec::new();
-    for i in 0..256 {
-        // let mut inner_vec: Vec<(u32, u32, u32, u32)> = Vec::new();
-        // let mut inner_vec = Vec::new();
-        for j in 0..256 {
-            let mut v = vector[i * 256 + j].clone();
-            if v < 0.0 {
-                v = 0.0;
-            } else if v > 1.0 {
-                v = 1.0;
+    for row in u.outer_iter() {
+        for e in row.iter() {
+            // let mut v = *e;
+            // if v < 0.0 {
+            //     v = 0.0;
+            // } else if v > 1.0 {
+            //     v = 1.0;
+            // } else {
+            // }
+            // v *= 255.0;
+            // let uv = v as u8;
+            let v = (if *e < 0.0 {
+                0.0
+            } else if *e > 1.0 {
+                1.0
             } else {
-            }
-            v = v * 255.0;
-            let uv = v as u8;
+                *e
+            } * 255.0) as u8;
 
-            texture_data.push(uv);
-            texture_data.push(uv);
-            texture_data.push(uv);
-            texture_data.push(uv);
+            texture_data.push(v);
+            texture_data.push(v);
+            texture_data.push(v);
+            texture_data.push(v);
         }
-        // texture_data.push(inner_vec);
     }
-    let image = glium::texture::RawImage2d::from_raw_rgba(texture_data, (256, 256));
-    image
+    glium::texture::RawImage2d::from_raw_rgba(texture_data, (256, 256))
 }
 
 fn draw_triangle(
@@ -90,157 +90,61 @@ fn draw_triangle(
     }
     implement_vertex!(Vertex, a_position, a_texcoord);
 
-    // #[derive(Copy, Clone)]
-    // struct VTexcoord {
-    //     v_texcoord: [f32; 2],
-    // }
-    // implement_vertex!(VTexcoord, v_texcoord);
     let vertex1 = Vertex {
         a_position: [-1.0, -1.0],
-        a_texcoord: [0.0, 0.0],
+        a_texcoord: [0.0, 1.0],
     };
     let vertex2 = Vertex {
-        a_position: [1.0, 1.0],
-        a_texcoord: [1.0, 0.0],
+        a_position: [1.0, -1.0],
+        a_texcoord: [1.0, 1.0],
     };
     let vertex3 = Vertex {
-        a_position: [1.0, -1.0],
+        a_position: [1.0, 1.0],
         a_texcoord: [1.0, 0.0],
     };
     let vertex4 = Vertex {
         a_position: [-1.0, -1.0],
-        a_texcoord: [0.0, 0.0],
+        a_texcoord: [0.0, 1.0],
     };
     let vertex5 = Vertex {
-        a_position: [1.0, 1.0],
-        a_texcoord: [1.0, 0.0],
-    };
-    let vertex6 = Vertex {
-        a_position: [-1.0, 1.0],
-        a_texcoord: [0.0, 1.0],
-    };
-    let vertex11 = Vertex {
-        a_position: [-1.0, -1.0],
-        a_texcoord: [0.0, 1.0],
-    };
-    let vertex12 = Vertex {
-        a_position: [1.0, -1.0],
-        a_texcoord: [1.0, 1.0],
-    };
-    let vertex13 = Vertex {
-        a_position: [1.0, 1.0],
-        a_texcoord: [1.0, 0.0],
-    };
-    let vertex14 = Vertex {
-        a_position: [-1.0, -1.0],
-        a_texcoord: [0.0, 1.0],
-    };
-    let vertex15 = Vertex {
         a_position: [-1.0, 1.0],
         a_texcoord: [0.0, 0.0],
     };
-    let vertex16 = Vertex {
+    let vertex6 = Vertex {
         a_position: [1.0, 1.0],
         a_texcoord: [1.0, 0.0],
     };
-    let shape = vec![vertex11, vertex12, vertex13, vertex14, vertex15, vertex16];
-    // let shape = vec![vertex1, vertex2, vertex3, vertex4, vertex5, vertex6];
-    // let shape = vec![vertex1, vertex2, vertex3];
-    // let shape = vec![vertex4, vertex5, vertex6];
+    let shape = vec![vertex1, vertex2, vertex3, vertex4, vertex5, vertex6];
 
     let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
 
-    // let a_texcoord1 = ATexcoord { a_texcoord: [0.0, 1.0] };
-    // let a_texcoord2 = ATexcoord { a_texcoord: [0.0, 0.0] };
-    // let a_texcoord3 = ATexcoord { a_texcoord: [1.0, 1.0] };
-    // let a_texcoord4 = ATexcoord { a_texcoord: [1.0, 0.0] };
-    // let a_texcoord5 = ATexcoord { a_texcoord: [1.0, 1.0] };
-    // let a_texcoord6 = ATexcoord { a_texcoord: [0.0, 0.0] };
-    // let a_shape = vec![
-    //     a_texcoord1,
-    //     a_texcoord2,
-    //     a_texcoord3,
-    //     a_texcoord4,
-    //     a_texcoord5,
-    //     a_texcoord6,
-    // ];
-    // let a_texcoord_buffer = glium::VertexBuffer::new(&display, &a_shape).unwrap();
-
     let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
 
-    // let v = include_str!("../res/shaders/matrix_visualizer_vertex.glsl");
-    // let f = include_str!("../res/shaders/matrix_visualizer_fragment.glsl");
     let program0 = glium::Program::from_source(
         &display,
         include_str!("../res/shaders/matrix_visualizer_vertex.glsl"),
         include_str!("../res/shaders/matrix_visualizer_fragment.glsl"),
         None,
     );
-    println!("{:?}", program0);
     let program = program0.unwrap();
-
-    // let vector = u.iter().map(|d| d).collect::<Vec<&f32>>();
-    // // println!("{:?}", vector);
-    // // let mut texture_data: Vec<Vec<(u32, u32, u32, u32)>> = Vec::new();
-    // let mut texture_data = Vec::new();
-    // for i in 0..256 {
-    //     // let mut inner_vec: Vec<(u32, u32, u32, u32)> = Vec::new();
-    //     // let mut inner_vec = Vec::new();
-    //     for j in 0..256 {
-    //         let mut v = vector[i * 256 + j].clone();
-    //         if v < 0.0 {
-    //             v = 0.0;
-    //         } else if v > 1.0 {
-    //             v = 1.0;
-    //         } else {
-    //         }
-    //         v = v * 255.0;
-    //         let uv = v as u8;
-    //
-    //         // inner_vec.push(vector[i + j].clone())
-    //         // inner_vec.push(vec![v, v, v, v]);
-    //         // inner_vec.push((uv, uv, uv, 1));
-    //         // inner_vec.push((0, 255, 255 as u32));
-    //         // inner_vec.push((255, 255, 255 as u32));
-    //         // inner_vec.push(255 as u32);
-    //         // texture_data.push(124 as u8);
-    //         // texture_data.push(125 as u8);
-    //         // texture_data.push(255 as u8);
-    //         // texture_data.push(255 as u8);
-    //         // texture_data.push(255 as u8);
-    //         texture_data.push(uv);
-    //         texture_data.push(uv);
-    //         texture_data.push(uv);
-    //         texture_data.push(uv);
-    //     }
-    //     // texture_data.push(inner_vec);
-    // }
-    // // let texture_data = vec![vec![0.0]];
-    //
-    // // let texture_data = [[1.0; 256]; 256];
-    // // let texture_data = u;
-    // // println!("{:?}", texture_data);
-    // // let texture = glium::texture::Texture2d::new(&display, texture_data).unwrap();
-    // println!("raw: {}", texture_data.len());
-    // let image = glium::texture::RawImage2d::from_raw_rgba(texture_data, (256, 256));
 
     let mut closed = false;
     while !closed {
-        // ラプラシアンの計算
-        let laplacian_u =
-            (roll(&u, 1, false) + roll(&u, -1, false) + roll(&u, 1, true) + roll(&u, -1, true)
-                - &u * 4.0) / (DX * DX);
-        let laplacian_v =
-            (roll(&v, 1, false) + roll(&v, -1, false) + roll(&v, 1, true) + roll(&v, -1, true)
-                - &v * 4.0) / (DX * DX);
+        for _ in 0..VISUALIZATION_STEP {
+            // ラプラシアンの計算
+            let laplacian_u =
+                (roll(&u, 1, false) + roll(&u, -1, false) + roll(&u, 1, true) + roll(&u, -1, true)
+                    - &u * 4.0) / (DX * DX);
+            let laplacian_v =
+                (roll(&v, 1, false) + roll(&v, -1, false) + roll(&v, 1, true) + roll(&v, -1, true)
+                    - &v * 4.0) / (DX * DX);
 
-        // Gray-Scottモデル方程式
-        let dudt = (laplacian_u * DU) - (&u * &v * &v) + F * (1.0 - &u);
-        let dvdt = (laplacian_v * DV) + (&u * &v * &v) - (F + K) * &v;
-        u = u + (DT as f32 * dudt);
-        v = v + (DT as f32 * dvdt);
-
-        // u = &u +         // u += DT * dudt;
+            // Gray-Scottモデル方程式
+            let dudt = (laplacian_u * DU) - (&u * &v * &v) + F * (1.0 - &u);
+            let dvdt = (laplacian_v * DV) + (&u * &v * &v) - (F + K) * &v;
+            u = u + (DT as f32 * dudt);
+            v = v + (DT as f32 * dvdt);
+        }
 
         let image = make_texture_image(&u);
         let texture = glium::texture::Texture2d::new(&display, image).unwrap();
@@ -248,34 +152,26 @@ fn draw_triangle(
         target.clear_color(1.0, 0.0, 0.0, 1.0);
         target
             .draw(
-                // (&vertex_buffer, &a_texcoord_buffer, &v_texcoord_buffer),
                 &vertex_buffer,
                 &indices,
                 &program,
-                // &glium::uniforms::EmptyUniforms,
-                // &uniform! {matrix: [0.0, 0.0, 0.0, 0.0]},
-                &uniform! {
-                u_texture: texture.sampled()
-                               },
+                &uniform! { u_texture: texture.sampled() },
                 &Default::default(),
             )
             .unwrap();
         target.finish().unwrap();
 
-        events_loop.poll_events(|event| match event {
-            glutin::Event::WindowEvent { event, .. } => match event {
-                glutin::WindowEvent::CloseRequested => closed = true,
-                _ => (),
-            },
-            _ => (),
+        events_loop.poll_events(|event| {
+            if let glutin::Event::WindowEvent { event, .. } = event {
+                if let glutin::WindowEvent::CloseRequested = event {
+                    closed = true
+                }
+            }
         });
     }
 }
 
-fn make_matrix() -> (
-    ndarray::ArrayBase<ndarray::OwnedRepr<f32>, ndarray::Dim<[usize; 2]>>,
-    ndarray::ArrayBase<ndarray::OwnedRepr<f32>, ndarray::Dim<[usize; 2]>>,
-) {
+fn make_matrix() -> (Matrix<f32>, Matrix<f32>) {
     // initialize
     let mut u = Array2::<f32>::ones((256, 256));
     let mut v = Array2::<f32>::zeros((256, 256));
@@ -283,10 +179,7 @@ fn make_matrix() -> (
     // 中央にSQUARE_SIZE四方の正方形を置く
     const SQUARE_SIZE: usize = 20;
     u.slice_mut(s![
-        // 0..256,
-        // 0..256,
         SPACE_GRID_SIZE / 2 - SQUARE_SIZE / 2..SPACE_GRID_SIZE / 2 + SQUARE_SIZE / 2,
-        // 0..256,
         SPACE_GRID_SIZE / 2 - SQUARE_SIZE / 2..SPACE_GRID_SIZE / 2 + SQUARE_SIZE / 2,
     ]).fill(0.5);
     v.slice_mut(s![
@@ -300,10 +193,10 @@ fn make_matrix() -> (
     u.add_assign(&u_rand);
     v.add_assign(&v_rand);
 
-    // println!("{:?}", u);
     (u, v)
 }
 
+// #[allow(clippy)]
 fn roll<A, T>(
     a: &ndarray::ArrayBase<ndarray::OwnedRepr<A>, ndarray::Dim<[usize; 2]>>,
     shift: T,
@@ -311,67 +204,40 @@ fn roll<A, T>(
 ) -> ndarray::ArrayBase<ndarray::OwnedRepr<A>, ndarray::Dim<[usize; 2]>>
 where
     A: Copy,
-    T: Integer + num_traits::cast::NumCast, // T: Integer + num_traits::cast::AsPrimitive<i32>, // T: Integer + std::ops::Neg + std::convert::From<std::ops::RangeFrom<T>>,
-                                            // ndarray::SliceOrIndex: std::convert::From<std::ops::RangeFrom<T>>
+    T: Integer + num_traits::cast::NumCast,
 {
     let shift: i32 = num::cast(shift).unwrap();
     let mut rotated = unsafe { Array2::uninitialized(a.dim()) };
-    match axis {
-        true => {
-            rotated
-                .slice_mut(s![.., ..shift])
-                .assign(&a.slice(s![.., -shift..]));
-            rotated
-                .slice_mut(s![.., shift..])
-                .assign(&a.slice(s![.., ..-shift]));
-        }
-        false => {
-            rotated
-                .slice_mut(s![..shift, ..])
-                .assign(&a.slice(s![-shift.., ..]));
-            rotated
-                .slice_mut(s![shift.., ..])
-                .assign(&a.slice(s![..-shift, ..]));
-        }
+    if axis {
+        rotated
+            .slice_mut(s![.., ..shift])
+            .assign(&a.slice(s![.., -shift..]));
+        rotated
+            .slice_mut(s![.., shift..])
+            .assign(&a.slice(s![.., ..-shift]));
+    } else {
+        rotated
+            .slice_mut(s![..shift, ..])
+            .assign(&a.slice(s![-shift.., ..]));
+        rotated
+            .slice_mut(s![shift.., ..])
+            .assign(&a.slice(s![..-shift, ..]));
     }
     rotated
 }
-// fn roll2(a: ndarray::ArrayBase<ndarray::OwnedRepr<f32>, ndarray::Dim<[usize; 2]>>) {
+
+#[allow(dead_code)]
 fn roll2() {
     let a = arr2(&[[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
-    // let mut a = Array2::<f32>::ones((256, 256));
     println!("{:?}", a);
     println!("{:?}", 1 - &a);
 
     let b = arr2(&[[2, 2, 2], [3, 3, 3], [4, 4, 4]]);
     println!("{:?}", &a * &b);
-    // println!("{:?}", a.slice(s![.., -2..]));
-    // println!("{:?}", a.slice(s![.., ..2]));
-    // println!("{:?}", a.slice(s![.., ..-2]));
-    // println!("{:?}", a.slice(s![.., 2..]));
-    // let mut b = unsafe { Array2::uninitialized(a.dim()) };
-    // b.slice_mut(s![.., ..2]).assign(&a.slice(s![.., -2..]));
-    // b.slice_mut(s![.., 2..]).assign(&a.slice(s![.., ..-2]));
-    // println!("{:?}", b);
-    //
-    // let mut c = unsafe { Array2::uninitialized(a.dim()) };
-    // c.slice_mut(s![..2, ..]).assign(&a.slice(s![-2.., ..]));
-    // c.slice_mut(s![2.., ..]).assign(&a.slice(s![..-2, ..]));
-    // println!("{:?}", c);
-}
 
-// impl<'a, P: PixelValue + Clone> Texture2dDataSource<'a> for Vec<Vec<[u32; 4]>> {
-//     type Data = P;
-//
-//     fn into_raw(self) -> RawImage2d<'a, P> {
-//         let width = self.iter().next().map(|e| e.len()).unwrap_or(0) as u32;
-//         let height = self.len() as u32;
-//
-//         RawImage2d {
-//             data: Cow::Owned(self.into_iter().flat_map(|e| e.into_iter()).collect()),
-//             width: width,
-//             height: height,
-//             format: <P as PixelValue>::get_format(),
-//         }
-//     }
-// }
+    for i in a.outer_iter() {
+        for j in i.iter() {
+            println!("{}", j)
+        }
+    }
+}
