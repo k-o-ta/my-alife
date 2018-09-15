81f9913d6b3cde059de19ac1ed98dfd9b7327062
Koji Ota <afterjnih@gmail.com>
2018-09-15 11:42:34 +0900

    add examples

---
+++ examples/chap02_gray_scott_spot.rs
@@ -0,0 +1,19 @@
+extern crate my_alife;
+
+use my_alife::algorithm::gray_scott::{initial_matrix, laplacian};
+use my_alife::visualizer::matrix_visualizer::MatrixVisualizer;
+use std::fmt::Debug;
+
+// model parameter
+const F: f32 = 0.035;
+const K: f32 = 0.065;
+
+fn main() -> Result<(), impl Debug> {
+    let (u, v) = initial_matrix();
+    let matrix = MatrixVisualizer::new(
+        "Gray Scott",
+        "res/shaders/matrix_visualizer_vertex.glsl",
+        "res/shaders/matrix_visualizer_fragment.glsl",
+    );
+    matrix?.draw((u, v), F, K, laplacian)
+}
